8c85ddef970b446519511dbeae956a9265f801a0
makerne2 <47746284+makerne2@users.noreply.github.com>
2019-11-27 15:16:16 -0300

    Add files via upload

---
+++ app.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once "biblioteca/acesso.php";
+require_once "biblioteca/alert.php";
+define('CONTROLADOR_PADRAO', 'produto'); //por padrão vem definido o controlador de paginas estáticas (paginasControlador)
+//define('URL_BASE', 'http://mundocountry.000webhostapp.com/index.php');
+define('URL_BASE', 'http://Localhost/MundoCountry/');
+//define('URL_BASE', 'mundocountry.atwebpages.com/');
+//define ('URL_BASE', 'http://MundoCountry.epizy.com/');
